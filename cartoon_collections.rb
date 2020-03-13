ca48997b5acf6a2ec516bfbed6f47aeda033ec50
Pablo Menz <p.menz.r@gmail.com>
2020-03-13 21:50:33 +0000

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,37 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(array) 
+  idx=0
+  until idx==array.length do        
+  [array].each do |nam|
+    n=idx+1
+  puts "#{n}.*#{nam[idx]}"  end
+  idx +=1
+  end
+end
+#--------------------------------------------------------------------------------
+
+def summon_captain_planet(array2)
+  array2.map!{|n| "#{n}".capitalize<<"!"}
+  
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+
+#--------------------------------------------------------------------------------
+def long_planeteer_calls(array3)
+  if array3.length<=4
+    true 
+  else
+    false 
+  end
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
-end
-
-def find_the_cheese# code an argument here
-  # the array below is here to help
+#--------------------------------------------------------------------------------
+def find_the_cheese(array4)
   cheese_types = ["cheddar", "gouda", "camembert"]
+  if array4.include?("cheddar"|| "gouda"|| "camembert")
+  shared = cheese_types & array4
+  return shared[0]
+  
+  else
+   
+end
 end
